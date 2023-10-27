697591745618b1976c60fe2f5630f83c57c262d2
yolitals <yolita2402@gmail.com>
2023-10-27 19:19:43 +0000

    Add security group

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+## Data para obtener el ID del AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -13,12 +14,27 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
+## Resource que crea una instance en AWS
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
+  }
+  security_groups = [aws_security_group.allow_http.name]
+}
+#Recurso que crea un security group en AWS
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+
+  ingress {
+    description      = "Http from internet"
+    to_port          = 80
+    from_port        = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
   }
 }
