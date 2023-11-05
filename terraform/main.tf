9cafd41043ada58b42c0e2a458cf9e710019cd1f
rosa-uwur <rsmaldonadov@gmail.com>
2023-11-05 16:24:08 -0600

    crear instancia ec2 y security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] 
+  owners = ["099720109477"]
 }
 ## Resource que crea una instance en AWS
 resource "aws_instance" "web" {
@@ -20,21 +20,21 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld2"
+    Name = "sandbox1"
   }
   security_groups = [aws_security_group.allow_http.name]
 }
 #Recurso que crea un security group en AWS
 resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
+  name        = "allow_http_sandbox1"
   description = "Allow http inbound traffic"
 
   ingress {
-    description      = "Http from internet"
-    to_port          = 80
-    from_port        = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "Http from internet"
+    to_port     = 80
+    from_port   = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~>5.23"
+    }
   }
-}
-required_version = ">=1.5"
+  required_version = ">=1.5"
 }
 provider "aws" {
   region = "us-east-1"
